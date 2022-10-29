9a0a426f0e1fdcd242752bcbe40ae72ab4417e2d
LewieM1995 <113435933+LewieM1995@users.noreply.github.com>
2022-10-29 15:41:28 +0100

    9th commit

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -8,13 +8,16 @@ class PagesController < ApplicationController
         params[:array1].split
         params[:array2].split
 
+
+        #array1 = correct list - manually collected
         @array1 = params[:array1].split.delete_if { |remove| remove == "70/0" || remove == "80/0" || remove == "5.5" || remove == "8.5" || remove == "70/4" || remove =="70/2" || remove == "3.5"}
         
+        #array2 = incorrect list 
         @array2 = params[:array2].split.delete_if { |remove| remove == "70/0" || remove == "80/0" || remove == "5.5" || remove == "8.5" || remove == "70/4" || remove =="70/2" || remove == "3.5"}
 
         @data = @array2.map(&:downcase) - @array1.map(&:downcase)
 
-        @result = @data
+        @result = @data.join(',  ')
         #debugger 
         #puts ""
     end
