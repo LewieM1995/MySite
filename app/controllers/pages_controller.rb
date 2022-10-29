4bf4d5db5c5fc60a8cc49e57ed28c54215454bdf
LewieM1995 <113435933+LewieM1995@users.noreply.github.com>
2022-10-29 08:38:15 +0100

    8th commit

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -8,8 +8,9 @@ class PagesController < ApplicationController
         params[:array1].split
         params[:array2].split
 
-        @array1 = params[:array1].split
-        @array2 = params[:array2].split
+        @array1 = params[:array1].split.delete_if { |remove| remove == "70/0" || remove == "80/0" || remove == "5.5" || remove == "8.5" || remove == "70/4" || remove =="70/2" || remove == "3.5"}
+        
+        @array2 = params[:array2].split.delete_if { |remove| remove == "70/0" || remove == "80/0" || remove == "5.5" || remove == "8.5" || remove == "70/4" || remove =="70/2" || remove == "3.5"}
 
         @data = @array2.map(&:downcase) - @array1.map(&:downcase)
 
