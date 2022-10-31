43c054faf8e241948f4b9402902eaa44efb5fc05
LewieM1995 <113435933+LewieM1995@users.noreply.github.com>
2022-10-31 19:10:46 +0000

    10th

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -5,17 +5,14 @@ class PagesController < ApplicationController
 
     def result
 
-        params[:array1].split
-        params[:array2].split
-
-
         #array1 = correct list - manually collected
-        @array1 = params[:array1].split.delete_if { |remove| remove == "70/0" || remove == "80/0" || remove == "5.5" || remove == "8.5" || remove == "70/4" || remove =="70/2" || remove == "3.5"}
+        @array1 = params[:array1].split.delete_if { |remove| remove == "70/0" || remove == "80/0" || remove == "5.5" || remove == "8.5" || remove == "70/4" || remove =="70/2" || remove == "3.5" || remove == "FSWF" || remove == "GVCT" || remove == "GVWF"}
         
         #array2 = incorrect list 
-        @array2 = params[:array2].split.delete_if { |remove| remove == "70/0" || remove == "80/0" || remove == "5.5" || remove == "8.5" || remove == "70/4" || remove =="70/2" || remove == "3.5"}
+        @array2 = params[:array2].split.delete_if { |remove| remove == "70/0" || remove == "80/0" || remove == "5.5" || remove == "8.5" || remove == "70/4" || remove =="70/2" || remove == "3.5" || remove == "FSWF" || remove == "GVCT" || remove == "GVWF"}
 
-        @data = @array2.map(&:downcase) - @array1.map(&:downcase)
+        @data = @array2.map(&:upcase) - @array1.map(&:upcase)
+
 
         @result = @data.join(',  ')
         #debugger 
